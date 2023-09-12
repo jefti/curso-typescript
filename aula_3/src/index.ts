80a79987fbbb6470ad55399b60474ff4fb2dd1cb
jefti <jeftimeira@gmail.com>
2023-09-12 14:19:36 -0300

    exercicios 1 e 2

---
+++ aula_3/src/index.ts
@@ -0,0 +1,21 @@
+function multiply(num1:number, num2:number) {
+    return num1 * num2;
+}
+
+function sum(num1:number, num2:number) {
+    return num1 + num2;
+}
+
+function isEven(num:number) {
+    return num % 2 === 0;
+}
+
+function showResult(result:any) {
+    if(isEven(result)) {
+        console.log(`The result is ${result} and it's even!`);
+    } else {
+        console.log(`The result is ${result} and it's not even!`);
+    }
+}
+
+showResult(sum(1,2));
