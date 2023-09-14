eda715dfbe1c427c8af7461460259be95ecfc4ad
jefti <jeftimeira@gmail.com>
2023-09-13 23:16:00 -0300

    aula 9

---
+++ aula_9/src/routers/music-router.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import musicController from "../controllers/music-controller";
+import { validateSchema } from "../middlewares/validateSchema.middleware";
+import { musicSchema } from "../schemas/music.schema";
+
+
+const musicRouter = Router();
+
+musicRouter.get("/musics", musicController.getMusics);
+musicRouter.post("/musics",validateSchema(musicSchema),musicController.createMusic); // TODO: validação via Joi
+
+export default musicRouter;
